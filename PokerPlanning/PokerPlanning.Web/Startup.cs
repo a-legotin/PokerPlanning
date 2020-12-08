3033952da191d06a4d7ed15a56e47eb9b114b27a
Alexander Legotin <mrproz@yandex.ru>
2020-12-08 21:21:28 +0300

    Dockerfile, startup fixed

--- PokerPlanning/PokerPlanning.Web/Startup.cs
+++ PokerPlanning/PokerPlanning.Web/Startup.cs
@@ -40,11 +40,9 @@ namespace PokerPlanning.Web
                 .AddIdentityServerJwt();
             services.AddControllersWithViews();
             services.AddRazorPages();
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -55,7 +53,6 @@ namespace PokerPlanning.Web
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
